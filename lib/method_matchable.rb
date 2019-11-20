e02d3c08fb7f50b91871c416096eb85f1d9c4543
kokuyouwind <kokuyouwind@gmail.com>
2019-11-20 20:09:58 +0900

    implement MethodMatchable

--- lib/method_matchable.rb
+++ lib/method_matchable.rb
@@ -1,6 +1,7 @@
 require "method_matchable/version"
 
 module MethodMatchable
-  class Error < StandardError; end
-  # Your code goes here...
+  def deconstruct_keys(keys)
+    keys.each_with_object({}) { |k, h| h[k] = send(k) }
+  end
 end
--- spec/method_matchable_spec.rb
+++ spec/method_matchable_spec.rb
@@ -1,5 +1,18 @@
 RSpec.describe MethodMatchable do
-  it "has a version number" do
-    expect(MethodMatchable::VERSION).not_to be nil
+  Person = Struct.new(:first_name, :last_name) do
+    include MethodMatchable
+
+    def full_name
+      "#{first_name} #{last_name}"
+    end
+  end
+
+  it 'match with methods' do
+    case Person.new('Hotaru', 'Shiragiku')
+    in { full_name: name }
+      expect(name).to eq('Hotaru Shiragiku')
+    else
+      fail
+    end
   end
 end
