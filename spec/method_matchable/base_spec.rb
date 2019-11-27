92db143b4fcdad59d137edc9ea96c2cedb206718
kokuyouwind <kokuyouwind@gmail.com>
2019-11-27 22:07:13 +0900

    support mixed mathing

--- spec/method_matchable/base_spec.rb
+++ spec/method_matchable/base_spec.rb
@@ -24,4 +24,40 @@ RSpec.describe MethodMatchable::Base do
       true
     end
   end
+
+  class Test
+    prepend MethodMatchable::Base
+
+    def deconstruct_keys(_keys)
+      { hash_key: :hash_val }
+    end
+
+    def method_key
+      :method_val
+    end
+  end
+
+  it 'can match with both super and method valuese' do
+    case Test.new
+    in { hash_key: v1, method_key: v2 }
+      expect(v1).to eq(:hash_val)
+      expect(v2).to eq(:method_val)
+    else
+      fail
+    end
+  end
+
+  class Hash
+    prepend MethodMatchable::Base
+  end
+
+  it 'not destroy original hash' do
+    h = { k: :v }
+    case h
+    in { k: v, size: size}
+      expect(v).to eq(:v)
+      expect(size).to eq(1)
+    end
+    expect(h).to eq({ k: :v })
+  end
 end
